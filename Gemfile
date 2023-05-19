6a6d78012e66ff8cfd98596240bbea18b51b3ae3
wilman orellano <wilmanjunior2001@gmail.com>
2023-05-18 22:42:50 -0500

    se configuro rails con turbo y se creo la tabla users y se añadio la funcion para autenticar a los usuarios

--- Gemfile
+++ Gemfile
@@ -70,3 +70,7 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem 'hotwire-rails'
+gem 'devise', '~> 4.9', '>= 4.9.2'
+gem 'simple_form', '~> 5.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+  devise_for :users, skip: [:sessions, :registrations]
+  as :user do
+    get 'signup', to: 'devise/registrations#new', as: :new_user_registration
+    post 'signup', to: 'devise/registrations#create', as: :user_registration
+    get 'signin', to: 'devise/sessions#new', as: :new_user_session
+    post 'signin', to: 'devise/sessions#create', as: :user_session
+    match 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
